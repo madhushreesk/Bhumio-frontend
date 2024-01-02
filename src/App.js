38a5f245272d18da05b2c037b05062be449454fb
madhushreesk <88674169+madhushreesk@users.noreply.github.com>
2024-01-03 00:19:57 +0530

    added typography

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import MuiTypography from "./components/MuiTypography";
+
 function App() {
   return (
     <div className="App">
       <h1>Patient details</h1>
+      <MuiTypography />
     </div>
   );
 }
